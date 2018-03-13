e4918d735209ce00e3a79a6d6572bb373a42fbee
Albert Moravec <albert.moravec@gmail.com>
2018-03-13 19:23:49 +0100

    Added enum for List Template types, addresses #3

---
+++ FluentlySharepoint/FluentlySharePoint/Enums/ListTemplate.cs
@@ -0,0 +1,16 @@
+﻿namespace KeenMate.FluentlySharePoint.Enums
+{
+	public enum ListTemplate
+	{
+		DocumentLibrary = 101,
+		Survey = 102,
+		Links = 103,
+		Announcements = 104,
+		Contacts = 105,
+		Events = 106,
+		Tasks = 107,
+		DiscussionBoard = 108,
+		PictureLibrary = 109,
+		DataSources = 110
+	}
+}
