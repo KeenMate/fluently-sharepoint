357bf3d70c34a8d1ef7840f2b0c21bdbca5dc33d
Albert Moravec <albert.moravec@gmail.com>
2018-04-09 18:10:32 +0200

    Added a few WebTemplate constants

---
+++ FluentlySharepoint/FluentlySharePoint/Constants/WebTemplates.cs
@@ -0,0 +1,17 @@
+﻿namespace KeenMate.FluentlySharePoint.Constants
+{
+	public static class WebTemplates
+	{
+		public const string GlobalTemplate = "GLOBAL#0";
+		public const string TeamSite = "STS#3";
+		public const string TeamSiteNew = TeamSite;
+		public const string TeamSiteClassic = "STS#0";
+		public const string BlankSite = "STS#1";
+		public const string DocumentWorkspace = "STS#2";
+		public const string WikiSite = "WIKI#0";
+		public const string Blog = "BLOG#0";
+		public const string DocumentCenter = "BDR#0";
+		public const string AppTemplate = "APP#0";
+		public const string AppCatalogSite = "APPCATALOG#0";
+	}
+}
