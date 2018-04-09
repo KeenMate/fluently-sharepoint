609af579076d0fd54b52b9f0ec013d83984c51c9
Albert Moravec <albert.moravec@gmail.com>
2018-04-09 16:28:09 +0200

    Added LCID enum

---
+++ FluentlySharepoint/FluentlySharePoint/Enums/Lcid.cs
@@ -0,0 +1,20 @@
+﻿namespace KeenMate.FluentlySharePoint.Enums
+{
+	public enum Lcid
+	{
+		English = 1033,
+		EnglishUS = 1033,
+		EnglishUK = 2057,
+		French = 1036,
+		Czech = 1029,
+		Slovak = 1051,
+		German = 1031,
+		Greek = 1032,
+		Japanese = 1041,
+		Polish = 1045,
+		Russian = 1049,
+		Spanish = 3082,
+		Swedish = 1053,
+		Chinese = 2052
+	}
+}
