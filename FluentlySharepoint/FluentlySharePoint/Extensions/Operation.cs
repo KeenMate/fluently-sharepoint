4d0afc077cd4de4442d84534171c18c348a43e2e
Albert Moravec <albert.moravec@gmail.com>
2018-04-20 16:52:37 +0200

    Added methods for managing Correlation ID

---
+++ FluentlySharepoint/FluentlySharePoint/Extensions/Operation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace KeenMate.FluentlySharePoint.Extensions
+{
+	public static class Operation
+	{
+		public static CSOMOperation SetCorrelationId(this CSOMOperation operation, Guid correlationId)
+		{
+			operation.CorrelationId = correlationId;
+			return operation;
+		}
+
+		public static CSOMOperation NewCorrelationId(this CSOMOperation operation)
+		{
+			operation.CorrelationId = new Guid();
+			return operation;
+		}
+
+		public static CSOMOperation RemoveCorrelationId(this CSOMOperation operation)
+		{
+			operation.CorrelationId = Guid.Empty;
+			return operation;
+		}
+	}
+}
