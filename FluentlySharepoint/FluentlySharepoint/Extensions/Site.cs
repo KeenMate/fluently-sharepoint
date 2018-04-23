ffcb9463f87261abd16fe389ca50307b6604323d
Albert Moravec <albert.moravec@gmail.com>
2018-04-23 19:17:05 +0200

    GetTerms method added

--- FluentlySharepoint/FluentlySharepoint/Extensions/Site.cs
+++ FluentlySharepoint/FluentlySharepoint/Extensions/Site.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using KeenMate.FluentlySharePoint.Enums;
 using Microsoft.SharePoint.Client;
+using Microsoft.SharePoint.Client.Taxonomy;
 
 namespace KeenMate.FluentlySharePoint.Extensions
 {
 	public static class Site
 	{
+		public static WebTemplateCollection GetWebTemplates(this CSOMOperation operation, Lcid lcid,
+			bool overrideCompatibilityLevel = false, Action<ClientContext, WebTemplateCollection> templatesLoader = null)
+		{
+			return GetWebTemplates(operation, (uint)lcid, overrideCompatibilityLevel, templatesLoader);
+		}
+
 		public static WebTemplateCollection GetWebTemplates(this CSOMOperation operation, uint lcid, bool overrideCompatibilityLevel = false, Action<ClientContext, WebTemplateCollection> templatesLoader = null)
 		{
 			operation.LogInfo($"Getting web templates for LCID {lcid}");
@@ -26,10 +36,28 @@ namespace KeenMate.FluentlySharePoint.Extensions
 			return collection;
 		}
 
-		public static WebTemplateCollection GetWebTemplates(this CSOMOperation operation, Lcid lcid,
-			bool overrideCompatibilityLevel = false, Action<ClientContext, WebTemplateCollection> templatesLoader = null)
+		public static TermCollection GetAllTerms(this CSOMOperation operation, string termSetName, Lcid lcid,
+			params Expression<Func<Term, object>>[] keysToLoad)
 		{
-			return GetWebTemplates(operation, (uint) lcid, overrideCompatibilityLevel, templatesLoader);
+			return operation.GetAllTerms(termSetName, (uint) lcid, keysToLoad);
+		}
+
+		public static TermCollection GetAllTerms(this CSOMOperation operation, string termSetName, uint lcid, params Expression<Func<Term, object>>[] keysToLoad)
+		{
+			var termSets = TaxonomySession.GetTaxonomySession(operation.Context).GetTermSetsByName(termSetName, (int) lcid);
+
+			operation.Context.Load(termSets);
+			operation.Execute();
+
+			if (termSets.Count == 0)
+				throw new KeyNotFoundException($"Term set {termSetName} not found");
+
+			var terms = termSets.First().GetAllTerms();
+
+			operation.Context.Load(terms, t => t.Include(keysToLoad));
+			operation.Context.ExecuteQuery();
+
+			return terms;
 		}
 	}
 }
